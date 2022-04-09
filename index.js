522502b2de66208a683e5d0b027afd570c73b44b
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-04-09 10:35:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ['Milo','Otis','Garfield']
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift()
+}
+
+// function appendCat(name){
+//     // myLength = cats.length()
+//     cats.splice(3,0,name)
+// }
+
+function prependCat(name){
+    newCats = cats.splice(0,0,name)
+}
+
+function removeFirstCat(){
+    newArray = cats.slice(1)
+}
+
+
+
